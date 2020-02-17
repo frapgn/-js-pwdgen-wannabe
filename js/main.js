2c9ee655995f6716a8e8224f95b1215a72bc57b1
Francesco Pagano <fra.pgn@protonmail.com>
2020-02-17 16:02:05 +0100

    es4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 var name = prompt('Qual è i ltuo nome?');
 var lastname = prompt('Qual è i ltuo cognome?');
 var favouriteColor = prompt('Qual è il tuo colore preferito?');
+// Es 1
 document.getElementById('pwdgen1').innerHTML = name + lastname + favouriteColor + '20';
-
 var year = new Date().getFullYear();
-console.log(year);
-// var year = date.getFullYear();
+// Es 2
 document.getElementById('pwdgen2').innerHTML = name + lastname + favouriteColor + year;
-
+// Es 3
 document.getElementById('pwdgen3').innerHTML = name + ';' + lastname + ';' + favouriteColor + ';' + year;
-
-document.getElementById('pwdgen4').innerHTML = 
+// Es 4
+document.getElementById('pwdgen4').innerHTML = name + ';' + lastname + ';' + favouriteColor + ';' + (year - 2000);
